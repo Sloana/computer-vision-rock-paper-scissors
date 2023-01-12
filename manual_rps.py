0a74688a8d04a7422101ef6279f7e0b43d29450f
Sloana <hajderal@lsbu.ac.uk>
2023-01-12 17:43:39 +0000

    Add files via upload

---
+++ manual_rps.py
@@ -0,0 +1,13 @@
+import random
+
+rps=["Rock","Paper","Scissors"]
+
+
+def get_computer_choice():
+   choice=random.choice(rps)
+    return choice
+
+
+def get_user_choice():
+    user_choice=input("Enter a choice")
+    return user_choice
