957cb2eec5eb5c243b8369341525184c5b8d9b8f
Sloana <hajderal@lsbu.ac.uk>
2023-01-13 19:33:39 +0000

    Update camera_rps.py

--- camera_rps.py
+++ camera_rps.py
@@ -1,12 +1,18 @@
 import cv2
+import time
 import keras
 from keras.models import load_model
 import numpy as np
+time.time()
 model = load_model('C:/Users/laura/OneDrive/Desktop/Computer Vision/keras_model.h5')
 cap = cv2.VideoCapture(0)
+time.time()
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 rps=["Rock","Paper","Scissors","Nothing"]
-while True: 
+print("cam")
+i=5
+while i>0: 
+    print("camera")
     ret, frame = cap.read()
     resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
     image_np = np.array(resized_frame)
@@ -23,7 +29,7 @@ while True:
     # Press q to close the window
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-            
+          
 # After the loop release the cap object
 cap.release()
 # Destroy all the windows
